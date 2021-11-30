3b81e3653fde2d7a4d3b4fd53220affebbd67487
Connor Kobriger <cjkobriger@gmail.com>
2021-11-30 14:53:10 -0700

    fixed login

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,19 +1,43 @@
 //*codes for 8 hours then dies*
 const User = require('../models/user')
+const { UnauthError } = require('../errors');
+const {BadRequestError} = require('../errors');
+
 
 const login = async (req, res) => {
-    res.send('login')
+
+    const { username, password } = req.body;
+    if (!username || !password) {
+        throw new BadRequestError("Enter an username AND a password.")
+    }
+
+    const userLogin = await User.findOne({ username });
+
+    if (!userLogin) {
+        throw new UnauthError('Invalid');
+    }
+
+    const isPasswordCorrect = await userLogin.comparePassword(password);
+    console.log(isPasswordCorrect);
+
+    if (!isPasswordCorrect) {
+        throw new UnauthError('Invalid')
+    }
+
+    const token = userLogin.createJWT();
+
+    res.json({ user: { name: newUser.username }, token });
 }
 
 const register = async (req, res) => {
     console.log(req.body);
     const newUser = await User.create(req.body);
     const token = newUser.createJWT();
-    res.json({user: {name: newUser.username}, token});
+    res.json({ user: { name: newUser.username }, token });
 }
 
-const favoriteJoke = async (req, res) => { 
+const favoriteJoke = async (req, res) => {
     res.send('joke saved')
 }
 
-module.exports = {login, register, favoriteJoke};
+module.exports = { login, register, favoriteJoke };
